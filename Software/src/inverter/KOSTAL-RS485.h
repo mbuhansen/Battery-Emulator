745dfe256b893337db275fc7f81e37aaa923868e
mbuhansen <91631350+mbuhansen@users.noreply.github.com>
2025-07-19 17:57:54 +0200

    Update KOSTAL-RS485.h

--- Software/src/inverter/KOSTAL-RS485.h
+++ Software/src/inverter/KOSTAL-RS485.h
@@ -5,14 +5,14 @@
 
 #define KOSTAL_SECONDARY_CONTACTOR
 
-#ifdef KOSTAL_SECONDARY_CONTACTOR
-#define SECONDARY_CONTACTOR_PIN 33
-#endif
-
 #define RS485_INVERTER_SELECTED
 #define RS485_BAUDRATE 57600
 //#define DEBUG_KOSTAL_RS485_DATA  // Enable this line to get TX / RX printed out via logging
 //#define DEBUG_KOSTAL_RS485_DATA_USB  // Enable this line to get TX / RX printed out via USB
+
+#ifdef KOSTAL_SECONDARY_CONTACTOR
+#define SECONDARY_CONTACTOR_PIN 33
+#endif
 
 #if defined(DEBUG_KOSTAL_RS485_DATA) && !defined(DEBUG_LOG)
 #error "enable LOG_TO_SD, DEBUG_VIA_USB or DEBUG_VIA_WEB in order to use DEBUG_KOSTAL_RS485_DATA"
