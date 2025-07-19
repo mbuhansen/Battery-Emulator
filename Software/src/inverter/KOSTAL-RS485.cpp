018244cf9173af378c5b39aa227481289f0503c4
mbuhansen <91631350+mbuhansen@users.noreply.github.com>
2025-07-19 18:13:21 +0200

    Update KOSTAL-RS485.cpp

--- Software/src/inverter/KOSTAL-RS485.cpp
+++ Software/src/inverter/KOSTAL-RS485.cpp
@@ -258,6 +258,7 @@ void update_RS485_registers_inverter() {
   // }
 
   // On startup, byte 59 seems to be always 0x02 couple of frames,.
+#ifdef KOSTAL_SECONDARY_CONTACTOR
   if (digitalRead(SECONDARY_CONTACTOR_PIN) == LOW) {
     CYCLIC_DATA[56] = 0x01;
     CYCLIC_DATA[59] = 0x00;
@@ -265,7 +266,14 @@ void update_RS485_registers_inverter() {
     CYCLIC_DATA[56] = 0x00;
     CYCLIC_DATA[59] = 0x02;
   }
-
+#else
+  if (datalayer.system.status.inverter_allows_contactor_closing) {
+    CYCLIC_DATA[56] = 0x01;
+    CYCLIC_DATA[59] = 0x00;
+  } else {
+    CYCLIC_DATA[56] = 0x00;
+    CYCLIC_DATA[59] = 0x02;
+  }
 #endif
 
   float2frame(CYCLIC_DATA, (float)datalayer.battery.status.max_discharge_current_dA / 10, 26);
