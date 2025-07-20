ecc1577bcf4da6b910383660edadce30cc5be7a6
mbuhansen <91631350+mbuhansen@users.noreply.github.com>
2025-07-20 17:17:12 +0200

    Update KOSTAL-RS485.h

--- Software/src/inverter/KOSTAL-RS485.h
+++ Software/src/inverter/KOSTAL-RS485.h
@@ -7,7 +7,7 @@
 
 #define RS485_INVERTER_SELECTED
 #define RS485_BAUDRATE 57600
-#define DEBUG_KOSTAL_RS485_DATA  // Enable this line to get TX / RX printed out via logging
+//#define DEBUG_KOSTAL_RS485_DATA  // Enable this line to get TX / RX printed out via logging
 //#define DEBUG_KOSTAL_RS485_DATA_USB  // Enable this line to get TX / RX printed out via USB
 
 #ifdef KOSTAL_SECONDARY_CONTACTOR
