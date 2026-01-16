d8b65448781f8ab3c94e4ce276eb248acc2c9de8
mbuhansen <91631350+mbuhansen@users.noreply.github.com>
2026-01-16 10:46:21 +0100

    Add contactor open after 8 info request.

--- Software/src/battery/BMW-I3-BATTERY.cpp
+++ Software/src/battery/BMW-I3-BATTERY.cpp
@@ -339,16 +339,16 @@ void BmwI3Battery::transmit_can(unsigned long currentMillis) {
     if (currentMillis - previousMillis20 >= INTERVAL_20_MS) {
       previousMillis20 = currentMillis;
 
-      //if (startup_counter_contactor < 160) {
-      //  startup_counter_contactor++;
-      //} else {                      //After 160 messages, turn on the request
-      //  BMW_10B.data.u8[1] = 0x10;  // Close contactors
-      //}
-      if (datalayer.system.status.inverter_allows_contactor_closing) {
-        BMW_10B.data.u8[1] = 0x10;  // Close contactors when inverter allows
-      } else {
-        BMW_10B.data.u8[1] = 0x00;  // Keep contactors open when inverter doesn't allow
+      if (startup_counter_contactor < 160) {
+        startup_counter_contactor++;
+      } else {                      //After 160 messages, turn on the request
+        BMW_10B.data.u8[1] = 0x10;  // Close contactors
       }
+      //if (datalayer.system.status.inverter_allows_contactor_closing) {
+      //  BMW_10B.data.u8[1] = 0x10;  // Close contactors when inverter allows
+      //} else {
+      //  BMW_10B.data.u8[1] = 0x00;  // Keep contactors open when inverter doesn't allow
+      //}
 
       BMW_10B.data.u8[1] = ((BMW_10B.data.u8[1] & 0xF0) + alive_counter_20ms);
       BMW_10B.data.u8[0] = calculateCRC(BMW_10B, 3, 0x3F);
